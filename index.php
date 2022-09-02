921be3f64dc95a563a26e1cae5f24693a5851880
bigeppo <carlog91cast@gmail.com>
2022-09-02 16:25:16 +0200

    add attribute in constructor

--- index.php
+++ index.php
@@ -5,10 +5,24 @@
     private $year;
     private $mainActor;
     private $genre;
+    private $oscar;
 
-    function__constructor($_movieName, $_director, $_year, $_mainActor, $_genre){
-        
+    public function __construct ($_movieName, $_director, $_year, $_mainActor, $_genre, $_oscar){
+        $this->movieName = $_movieName;
+        $this->director = $_director;
+        $this->year = $_year;
+        $this->mainActor = $_mainActor;
+        $this->genre = $_genre;
+    }
+    public function setName($movieName){
+        $this->movieName = $movieName;
+        var_dump($movieName);
     }
   }
+
+  $movieUno = new Movie('Avatar', '2012', 'Osvaldo Paniccia', '2012', 'Nino Frassica', 'snuff movie', '1');
+
+  var_dump($movieUno);
+
 
 ?>
