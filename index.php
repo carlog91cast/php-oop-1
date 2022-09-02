843b0ccc616b983037fc8f5ac9eb92971b10d2d0
bigeppo <carlog91cast@gmail.com>
2022-09-02 15:54:54 +0200

    add class Movie; add contructor

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+  class Movie {
+    private $movieName;
+    private $director;
+    private $year;
+    private $mainActor;
+    private $genre;
+
+    function__constructor($_movieName, $_director, $_year, $_mainActor, $_genre){
+        
+    }
+  }
+
+?>
